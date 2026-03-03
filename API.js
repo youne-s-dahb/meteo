0d54da52f983ca50c817576224098945be2237b4
Youne-s-dahbi <dahyoubi123@gmail.com>
2026-03-03 09:37:16 +0000

    test performance mobile

--- API.js
+++ API.js
@@ -9,8 +9,8 @@ function data(){
                         lat = position.coords.latitude;
                         lon = position.coords.longitude;
 
-                        xhr2.open('GET',`https://nominatim.openstreetmap.org/reverse?format=json&lat=${lat}&lon=${lon}`);
-                        xhr2.send();
+                        //xhr2.open('GET',`https://nominatim.openstreetmap.org/reverse?format=json&lat=${lat}&lon=${lon}`);
+                        //  xhr2.send();
 
                         xhr.open('GET',`https://api.open-meteo.com/v1/forecast?latitude=${lat}&longitude=${lon}&current=temperature_2m,relative_humidity_2m,apparent_temperature,is_day,weather_code,wind_speed_10m&hourly=temperature_2m,precipitation_probability,weather_code,uv_index,visibility&daily=weather_code,temperature_2m_max,temperature_2m_min,sunrise,sunset,precipitation_sum&timezone=auto`)
                         xhr.send();
